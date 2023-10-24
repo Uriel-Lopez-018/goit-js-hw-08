7eab3f2cf234c1ce717265ba145dfa684e4898f9
Uriel-Lopez-018 <uriellopezpacheco5875@gmail.com>
2023-10-23 22:01:23 -0600

    cambios en gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,9 +2,9 @@ import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const galleryEl = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
 
-galleryEl.insertAdjacentHTML('beforeend', renderGalleryItems());
+gallery.insertAdjacentHTML('beforeend', renderGalleryItems());
 
 function renderGalleryItems() {
   return galleryItems
